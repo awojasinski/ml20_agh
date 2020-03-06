0c523f81ade103e4db3815bfd89f66c34dd6f1f6
awojasinski <a.wojasinski@gmail.com>
2020-03-06 13:49:05 +0100

    lab2 init

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,62 +1,56 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
 
 import os
-
 path = os.getcwd() + '\\dane1.txt'
-data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
 
 
 def computeCost(X, y, theta):
-    h = X @ np.transpose(theta)
-    diff_yk = h - y
-    square = np.power(diff_yk, 2)
-    cost =  sum(square)/(len(y)*2)
-    
+    h = X @ theta.T
+    cost = sum(np.square(h - y)) / (X.shape[0] * 2)
+
     return cost
 
 
 def gradient_prosty(X, y, theta, alpha, it):
-    #cost = np.empty()
-
+    cost = np.empty(shape=([0, 1]))
+    dt_theta = np.empty(shape=(theta.size,), dtype=float)
     for i in range(it):
-        h = X @ np.transpose(theta)
-        dt_theta_0 = sum(h-y) / len(y)
-        dt_theta_1 = sum((h - y) * X) / len(y)
-        print(dt_theta_0.shape)
-        print(dt_theta_1.shape)
-        theta[0] -= alpha @ dt_theta_0
-        theta[1] -= alpha @ dt_theta_1
-        cost.append(computeCost(X, y, theta))
+        h = X @ theta.T
+        for j in range(theta.size):
+            dt_theta[j] = (h - y).T @ X[:, j] / X.shape[0]
+        theta -= alpha * dt_theta
+        cost = np.append(cost, [computeCost(X, y, theta)])
 
-        return cost, theta
+    return cost, theta
 
 
-'''
-print(data.head)
-print(data.describe())
-'''
+data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
+population = data['Population']
+profit = data['Profit']
 
-#data = data.sort_values('Population')
-'''
-plt.plot(data['Population'], data['Profit'], 'o')
+plt.plot(population, profit, 'o')
 plt.xlabel('Population')
 plt.ylabel('Profit')
-plt.grid(True)
 plt.show()
-'''
 
-data.insert(0, "No.", range(data.shape[0]), True)
+X = np.array([population]).T
+X = np.append(np.full(shape=(data.shape[0], 1), fill_value=1), X, axis=1)
+y = np.array([profit]).T
+theta = np.array([[0, 0]], dtype=float)
 
-X = data[['No.', 'Population']].copy()
-y = data['Profit'].copy()
+print(computeCost(X, y, theta))
 
-X = np.matrix(X.values)
-y = np.matrix(y.values)
-theta = np.matrix(np.array([0,0]))
+iterations = 1000
+cost, theta = gradient_prosty(X, y, theta, alpha=0.01, it=iterations)
+print(theta)
 
-print(computeCost(X, np.transpose(y), theta))
-cost, theta = gradient_prosty(X, np.transpose(y), theta, alpha=0.01, it=1000) 
-plt.plot_surface(theta[0], theta[1], cost)
+plt.plot(range(iterations), cost)
+plt.title('Cost function')
+plt.xlabel('Iterations')
+plt.ylabel('Cost')
 plt.show()
+
+
+
