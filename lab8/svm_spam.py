5f59ea6a65e66f99942d137652022c26a4da24a4
awojasinski <a.wojasinski@gmail.com>
2020-04-22 12:30:10 +0200

    main script completed

--- lab8/svm_spam.py
+++ lab8/svm_spam.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 import numpy as np
 from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 from process_email import process_email
 from email_features import email_features
@@ -38,7 +37,7 @@ print('Word Indices: \n')
 print(word_indices)
 print('\n\n')
 
-# input('Program paused. Press enter to continue.\n')
+#input('Program paused. Press enter to continue.\n')
 
 # %% ==================== Part 2: Feature Extraction ====================
 #  Now, you will convert each email into a vector of features in R^n. 
@@ -56,7 +55,7 @@ features = email_features(word_indices)
 print('Length of feature vector: {}\n'.format(len(features[0])))
 print('Number of non-zero entries: {}\n'.format(sum(sum(features > 0))))
 
-# input('Program paused. Press enter to continue.\n')
+#input('Program paused. Press enter to continue.\n')
 
 # %% =========== Part 3: Train Linear SVM for Spam Classification ========
 #  In this section, you will train a linear classifier to determine if an
@@ -73,37 +72,35 @@ print('The training dataset was loaded.')
 print('\nTraining Linear SVM (Spam Classification)\n')
 print('(this may take 1 to 2 minutes) ...\n')
 
-# FIXME: Create a linear SVC classifier (with C = 0.1).
-clf = None
+# Create a linear SVC classifier (with C = 0.1).
+clf = svm.SVC(kernel='linear', C=0.1, random_state=1, probability=True)
 
-# FIXME: Fit the SVC model using the training data.
+# Fit the SVC model using the training data.
+clf.fit(X_train, y_train)
 
-# FIXME: Predict the labelling.
-y_pred = None
+# Predict the labelling.
+y_pred = clf.predict(X_train)
 
-# FIXME: Compute the training accuracy.
-acc_train = None
+# Compute the training accuracy.
+acc_train = accuracy_score(y_train, y_pred)
 print('Training Accuracy: {:.2f}%\n'.format(acc_train * 100))
 
 # %% =================== Part 4: Test Spam Classification ================
 #  After training the classifier, we can evaluate it on a test set.
 
-# FIXME: Load the test dataset ('data/spamTest_X.csv', 'data/spamTest_y.csv').
+# Load the test dataset ('data/spamTest_X.csv', 'data/spamTest_y.csv').
 # You will have Xtest, ytest in your environment
-X_test = None
-y_test = None
+X_test = np.genfromtxt('data/spamTest_X.csv', delimiter=',')
+y_test = np.genfromtxt('data/spamTest_y.csv', delimiter=',')
 
 print('\nEvaluating the trained Linear SVM on a test set ...\n')
 
-# FIXME: Predict the labelling.
-y_pred = None
+# Predict the labelling.
+y_pred = clf.predict(X_test)
 
-# FIXME: Compute the training accuracy.
-acc_test = None
+# Compute the training accuracy.
+acc_test = accuracy_score(y_test, y_pred)
 print('Test Accuracy: {:.2f}%\n'.format(acc_test * 100))
-
-# input('Program paused. Press enter to continue.\n')
-
 
 # %% ================= Part 5: Top Predictors of Spam ====================
 #  Since the model we are training is a linear SVM, we can inspect the
@@ -112,25 +109,17 @@ print('Test Accuracy: {:.2f}%\n'.format(acc_test * 100))
 #  the highest weights in the classifier. Informally, the classifier
 #  'thinks' that these words are the most likely indicators of spam.
 
-# FIXME: Print the list of 15 most prominent features.
-# (i.e. words which gives strongest evidence for an email being a spam)
-# - Obtain the weights of the SVC model.
-# - Obtain the indices that would sort the weights in the descending order.
-# - Obtain the vocabulary.
+weights = clf.coef_
+idx = np.argsort(weights[0, :])
 
-weights = None
-idx = None
-
-vocabulary_dict = None
+vocabulary_dict = get_vocabulary_dict()
 
 print('\nTop predictors of spam: \n')
 for i in range(15):
-    # FIXME: Replace each `None` with an appropriate expression.
     print(' {word:<20}: {weight:10.6f}'.format(
-        word=None, weight=None))
+        word=vocabulary_dict[idx[-(i+1)]+1], weight=weights[0, idx[-(i+1)]]))
 
 print('\n\n')
-# input('\nProgram paused. Press enter to continue.\n')
 
 # %% =================== Part 6: Try Your Own Emails =====================
 #  Now that you've trained the spam classifier, you can use it on your own
@@ -149,8 +138,7 @@ filename = 'data/spamSample1.txt'
 file_contents = read_file(filename)
 word_indices = process_email(file_contents)
 x = email_features(word_indices)
-# FIXME: Predict the labelling.
-y_pred = None
+y_pred = clf.predict(x)
 
 print('\nProcessed {}\n\nSpam Classification: {}\n'.format(filename, y_pred[0] > 0))
 print('(1 indicates spam, 0 indicates not spam)\n\n')
